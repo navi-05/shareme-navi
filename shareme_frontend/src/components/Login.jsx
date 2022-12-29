a9d127d17a404c2bbc7c85b1f957e66c9ca6fc84
navi-05 <naveenthatsmyname5@gmail.com>
2022-12-29 11:43:02 +0530

    first-commit

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { useGoogleLogin } from '@react-oauth/google'
+
+import { useNavigate } from 'react-router-dom'; 
+import { FcGoogle } from 'react-icons/fc';
+
+import axios from 'axios';
+
+import shareVideo from '../assets/share.mp4'; 
+import logo from '../assets/logowhite.png'; 
+
+import { client } from '../client';
+
+
+const Login = () => {
+
+  const navigate = useNavigate(); 
+
+  const login = useGoogleLogin({
+    onSuccess: async response => {
+      try{
+        const res = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+          "Authorization": `Bearer ${response.access_token}`
+        }
+      })
+      localStorage.setItem('user', JSON.stringify(res.data))
+      const {name, sub, picture}   = res.data; 
+
+        const doc = {
+          _id: sub,
+          _type: 'user', 
+          userName: name,
+          image: picture,
+        }
+
+        client.createIfNotExists(doc)
+          .then(() => {
+            navigate('/', { replace: true }); 
+          })
+
+      } catch (err) {
+        console.log(err); 
+      }
+    },
+    onError: 'Please try again later'
+  });
+  
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+        <div className='relative w-full h-full'>
+            <video 
+             src={shareVideo}
+             type="video/mp4"
+             loop
+             controls={false}
+             muted
+             autoPlay
+             className='w-full h-full object-cover'
+            />
+            <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                <div className='p-5'>
+                    <img src={logo} width="130px" alt="logo" />
+                </div>
+                <div className='shadow-2xl'>
+                    <button 
+                     className='bg-mainColor flex justify-center items-center p-4 rounded-2xl '
+                     onClick={() => login()}
+                     type="button"
+                    >
+                     <FcGoogle className='mr-4' /> Sign in with Google 
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ shareme_frontend/src/container/Pins.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Feed from '../components/Feed'
+import CreatePin from '../components/CreatePin'
+import Navbar from '../components/Navbar'
+import Search from '../components/Search'
+import PinDetail from '../components/PinDetail'
+
+const Pins = ({ user }) => {
+
+  const [searchTerm, setSearchTerm] = useState('')
+
+  return (
+    <div className='px-2 md:px-5'>
+      <div className='bg-gray-50'>
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user && user}/>
+      </div>
+
+      <div className='h-full'>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetail user={user}/>} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default Pins
